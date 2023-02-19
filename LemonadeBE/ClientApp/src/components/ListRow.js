b5898a6c91e933ed9d83ab5790c4dc229c475861
MRLmic <missi.luna@gmail.com>
2023-02-18 19:46:44 -0700

    Add lemon icon, add row component

---
+++ LemonadeBE/ClientApp/src/components/ListRow.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Lemon from './../img/lemon.png'
+
+const ListRow = () => {
+
+    return (<tr>
+                <td>
+                    <img src={Lemon} alt='lemon'></img>
+                </td>
+                <td>
+                    <span>
+                        
+                    </span>
+                </td>
+                </tr>)
+};
+
+export default ListRow;
