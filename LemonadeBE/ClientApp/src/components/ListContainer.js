b5855639ebe27a0583c9bcde26eafc7b86e1e2a3
MRLmic <missi.luna@gmail.com>
2023-02-18 18:11:46 -0700

    Set up basic table header appearance

--- LemonadeBE/ClientApp/src/components/ListContainer.js
+++ LemonadeBE/ClientApp/src/components/ListContainer.js
@@ -1,7 +1,56 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import MaterialReactTable from 'material-react-table';
+import { data } from '../stub.js'
 
 const ListContainer = () => {
-  return <div className='List-container'>Hello, List Container</div>
+
+  const columns = useMemo(
+    () => [
+      {
+        accessorKey: 'data.Id', //access nested data with dot notation
+        header: '',
+      },
+      {
+        accessorKey: 'data.userFirst',
+        header: '',
+      },
+      {
+        accessorKey: 'data.userFirst',
+        header: 'Price',
+      },
+      {
+        accessorKey: 'data.userFirst',
+        header: 'QTY',
+      },
+      {
+        accessorKey: 'data.userFirst',
+        header: 'Total',
+      },
+      {
+        accessorKey: 'data.userFirst',
+        header: '',
+      },
+    ],
+    []
+  );
+
+return <div className='List-container'>
+  {/* {data.map((data, key) => {
+    return (
+      <div key={key}>
+        {data.Id}
+      </div>
+    )
+  })} */}
+  <MaterialReactTable
+    columns={columns}
+    data={data}
+    enableColumnActions={false}
+    enablePagination={false}
+    enableSorting={false}
+    enableTopToolbar={false}
+  />
+</div>
 }
 
 export default ListContainer;
