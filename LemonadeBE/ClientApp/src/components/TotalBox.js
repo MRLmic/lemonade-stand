07ac21758415a380d89059a7cc6f258d1a3550dd
MRLmic <missi.luna@gmail.com>
2023-02-20 11:34:58 -0700

    formatting

--- LemonadeBE/ClientApp/src/components/TotalBox.js
+++ LemonadeBE/ClientApp/src/components/TotalBox.js
@@ -2,14 +2,11 @@ import React from 'react';
 
 const TotalBox = () => {
     return <div className='total-box'>
-        
-        <div className='total'>Total</div>
-            <div className='amount'>$5.00
-            
-        </div>
-        <br></br>
-        <button className='order-button'>Order Now</button>
-    </div>;
+                <div className='total'>Total</div>
+                <div className='amount'>$5.00</div>
+                <br></br>
+                <button className='order-button'>Order Now</button>
+            </div>;
 };
 
 export default TotalBox;
