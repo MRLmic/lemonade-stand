7d88d4d320be7b45893119de964d35b4e67a3698
MRLmic <missi.luna@gmail.com>
2023-02-25 14:16:50 -0700

    Create Context

---
+++ LemonadeBE/LemonadeContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace LemonadeBE;
+public class LemonadeContext: DbContext
+{
+        public DbSet<Order> Orders => Set<Order>();
+        public DbSet<Customer> Customers => Set<Customer>();
+        public DbSet<Product> Products => Set<Product>();
+        public LemonadeContext(DbContextOptions<LemonadeContext> options) : base(options) {}
+}
+  
--- LemonadeBE/Program.cs
+++ LemonadeBE/Program.cs
@@ -1,8 +1,9 @@
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<LemonadeBE.LemonadeContext>(options => options.UseSqlServer("Server=tcp:jh-dev-products.database.windows.net,1433;Initial Catalog=Products;Persist Security Info=False;User ID=JHAdmin;Password=Puppies1234;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Connection Timeout=30;"));
 
 var app = builder.Build();
 
