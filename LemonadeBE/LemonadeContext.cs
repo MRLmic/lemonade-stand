d30b7cedf0f0c7426e29b814c5d796114233716c
MRLmic <missi.luna@gmail.com>
2023-02-25 20:01:20 -0700

    configure connection strings

--- LemonadeBE/LemonadeContext.cs
+++ LemonadeBE/LemonadeContext.cs
@@ -7,5 +7,14 @@ public class LemonadeContext: DbContext
         public DbSet<Customer> Customers => Set<Customer>();
         public DbSet<Product> Products => Set<Product>();
         public LemonadeContext(DbContextOptions<LemonadeContext> options) : base(options) {}
+                    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("appsettings.json")
+            .Build();
+        optionsBuilder.UseSqlServer(configuration.GetConnectionString("LemonadeContext"));
+    }
+        
 }
   
