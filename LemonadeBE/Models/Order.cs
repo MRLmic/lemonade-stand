7afa5fa94b7e205db8162bf3cf66340c67f47c2c
MRLmic <missi.luna@gmail.com>
2023-02-25 18:52:26 -0700

    Add additional attributes to Order class for customer data

--- LemonadeBE/Models/Order.cs
+++ LemonadeBE/Models/Order.cs
@@ -7,7 +7,8 @@ public class Order
 {
     [Key]
     public int OrderId { get; set; }
-    public string Customer { get; set; }
-
+    public string CustomerName { get; set; }
+    public string CustomerContact {get;set;}
+    public string ContactType {get; set;}
     public virtual ICollection<Product> Products { get; set; }
-    }
+}
