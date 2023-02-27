aa41a07ae318e5fa685daa97e385badb85dd2e6f
MRLmic <missi.luna@gmail.com>
2023-02-26 22:32:35 -0700

    Add Item Name

--- LemonadeBE/Migrations/20230227033345_AddItemNameColumn.Designer.cs
+++ LemonadeBE/Migrations/20230227033345_AddItemNameColumn.Designer.cs
@@ -4,6 +4,7 @@ using LemonadeBE;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LemonadeBE.Migrations
 {
     [DbContext(typeof(LemonadeContext))]
-    partial class LemonadeContextModelSnapshot : ModelSnapshot
+    [Migration("20230227033345_AddItemNameColumn")]
+    partial class AddItemNameColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
