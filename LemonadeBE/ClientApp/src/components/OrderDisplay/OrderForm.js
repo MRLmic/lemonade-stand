29538f49b8e27389313fb3d52e316bd416c1a81a
MRLmic <missi.luna@gmail.com>
2023-02-28 10:32:13 -0700

    Remove unneeded using

--- LemonadeBE/ClientApp/src/components/OrderDisplay/OrderForm.js
+++ LemonadeBE/ClientApp/src/components/OrderDisplay/OrderForm.js
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect, useRef, useState} from 'react';
+import React, {useContext, useEffect, useRef} from 'react';
 import {TotalContext} from './../Wrapper.js'
 
 export default function OrderForm({open,setOpen, submit}) {
