b4a7d51d78a6445f82162e35637b4e78b14dda01
MRLmic <missi.luna@gmail.com>
2023-02-28 01:48:43 -0700

    OrderResponse object

---
+++ LemonadeBE/OrderResponse.cs
@@ -0,0 +1,6 @@
+public class OrderResponse {
+    public IDictionary<string, int> Orders { get; set; }    
+    public string CustomerName { get; set; }
+    public string CustomerContact { get; set; }
+    public string ContactType { get; set; }
+}
